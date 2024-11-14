f996f272b19d8c0eb23d474d2e495bb9e16bdc9a
Siwar Taktak <siwar.taktak@esprit.tn>
2024-11-14 13:59:16 +0100

    Fix:FormValidation + Layout Signup

--- src/app/components/auth/sign-up/sign-up.component.spec.ts
+++ src/app/components/auth/sign-up/sign-up.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUpComponent } from './sign-up.component';
 
+
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
