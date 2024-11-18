98a32c7f123aae53e3fa84955a7ad1874c891298
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2024-11-18 12:05:45 +0100

    add: 1st version of product management page , sorting and filternig working

--- src/app/components/admin/products-management/products-management.component.ts
+++ src/app/components/admin/products-management/products-management.component.ts
@@ -1,64 +1,126 @@
-import {Component, OnInit} from '@angular/core';
-import {ProductService} from '../../../services/product.service';
-import {Product} from '../../../models/product';
-import {Observable} from 'rxjs';
-import {AsyncPipe, DatePipe, NgForOf, NgOptimizedImage} from '@angular/common';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../../models/product';
+import { MatPaginator } from '@angular/material/paginator';
+import {MatSort, Sort} from '@angular/material/sort';
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable,
+  MatTableDataSource
+} from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
-import {UpdateProductComponent} from './update-product/update-product.component'
+import { UpdateProductComponent } from './update-product/update-product.component';
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
+import {MatOption, MatSelect, MatSelectModule} from '@angular/material/select';
+import {MatInput, MatInputModule} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {NgForOf} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+
 @Component({
   selector: 'app-products-management',
   standalone: true,
   imports: [
+    MatPaginator,
+    MatSort,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatInput,
+    MatButton,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
     NgForOf,
-    DatePipe,
-    AsyncPipe,
-    HttpClientModule,
-    NgOptimizedImage
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    HttpClientModule
   ],
-  providers:[ProductService],
+  providers: [ProductService],
   templateUrl: './products-management.component.html',
-  styleUrl: './products-management.component.css'
+  styleUrls: ['./products-management.component.css']
 })
-export class ProductsManagementComponent implements OnInit{
-  products: Observable<Product[]> = new Observable<Product[]>();
-  constructor(private productService: ProductService,private http: HttpClient,private dialog: MatDialog) {}
+export class ProductsManagementComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'price', 'description', 'actions'];
+  dataSource = new MatTableDataSource<Product>();
+  categories: string[] = ['Electronics', 'Clothing', 'Food'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild('nameInput') nameInput!: ElementRef;
+
+  nameFilter: string = '';
+  categoryFilter: string = '';
+  priceSort: string = 'asc';
+
+  constructor(private productService: ProductService, private dialog: MatDialog) {}
+
   ngOnInit() {
-    this.products = this.productService.getAllProducts();
+    this.loadProducts();
+    this.dataSource.filterPredicate = (data: Product, filter: string) => {
+      return data.name.trim().toLowerCase().includes(filter);
+    };
   }
+
+  loadProducts() {
+    this.productService.getAllProducts().subscribe(products => {
+      this.dataSource.data = products;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+  applyPriceSort() {
+    const sortState: Sort = { active: 'price', direction: this.priceSort as 'asc' | 'desc' };
+    this.dataSource.sort!.active = sortState.active;
+    this.dataSource.sort!.direction = sortState.direction;
+    this.dataSource.sort!.sortChange.emit(sortState);
+  }
+  applyNameFilter(event: any) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = inputValue.trim().toLowerCase();
+  }
+
+  applyCategoryFilter() {
+    this.dataSource.filterPredicate = (data: Product, filter: string) => {
+      return this.categoryFilter ? data.category === this.categoryFilter : true;
+    };
+    this.dataSource.filter = this.categoryFilter;
+  }
+
+  clearFilters() {
+    this.nameFilter = '';
+    this.categoryFilter = '';
+    this.dataSource.filter = '';
+    this.nameInput.nativeElement.value = '';
+  }
+
   onDelete(product: Product) {
     if (confirm('Are you sure you want to delete this product?')) {
-      this.productService.deleteProduct(product).subscribe(
-        () => {
-          location.reload();
-        },
-        (error) => {
-          console.error('Error deleting product:', error);
-        }
-      );
+      this.productService.deleteProduct(product).subscribe(() => this.loadProducts());
     }
   }
 
   onUpdate(product: Product) {
     const dialogRef = this.dialog.open(UpdateProductComponent, {
       width: '400px',
-      data: product  // Pass the selected product data to the dialog
+      data: product
     });
 
     dialogRef.afterClosed().subscribe((updatedProduct) => {
       if (updatedProduct) {
-        // Now use the updated product returned from the dialog
-        this.productService.updateProduct(updatedProduct).subscribe(
-          (response) => {
-            console.log('Product updated successfully:', response);
-            location.reload();
-          },
-          (error) => {
-            console.error('Error updating product:', error);
-          }
-        );
+        this.productService.updateProduct(updatedProduct).subscribe(() => this.loadProducts());
       }
     });
   }
-
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,23 +7,20 @@ import { Product } from '../models/product';
     providedIn: 'root'
   })
   export class ProductService {
-    private jsonUrl = 'http://localhost:3000/products';
+    private jsonUrl = 'http://localhost:3001/products';
 
     constructor(private http: HttpClient) {}
 
     getAllProducts(): Observable<Product[]> {
-      console.log(this.jsonUrl)
       return this.http.get<Product[]>(this.jsonUrl);
 
     }
     updateProduct(product: Product): Observable<Product> {
       const url = `${this.jsonUrl}/${product.id}`;
-      console.log(url);
       return this.http.put<Product>(url, product);
     }
     deleteProduct(product: Product): Observable<Product> {
       const url = `${this.jsonUrl}/${product.id}`;
-      console.log(url);
       return this.http.delete<Product>(url);
     }
 
