98298f19576792269b522f4f54d2cdd66054adcb
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2024-11-19 11:09:52 +0100

    add: column category for admin + filter by category

--- src/app/components/admin/products-management/products-management.component.ts
+++ src/app/components/admin/products-management/products-management.component.ts
@@ -53,7 +53,7 @@ import {HttpClientModule} from '@angular/common/http';
   styleUrls: ['./products-management.component.css']
 })
 export class ProductsManagementComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'price', 'description', 'actions'];
+  displayedColumns: string[] = ['id', 'name', 'price', 'description','category', 'actions'];
   dataSource = new MatTableDataSource<Product>();
   categories: string[] = ['Electronics', 'Clothes', 'Books', 'Home', 'Sports'];
 
