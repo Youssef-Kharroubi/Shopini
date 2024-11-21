de8580966cf6eda99bdfe8afccefc430a4b8f04f
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2024-11-21 14:52:06 +0100

    add: category selector for updating products

--- src/app/components/admin/products-management/update-product/update-product.component.ts
+++ src/app/components/admin/products-management/update-product/update-product.component.ts
@@ -2,19 +2,24 @@ import { Component,Inject,OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Product } from '../../../../models/product';
 import {FormsModule} from '@angular/forms';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-update-product',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatSelect,
+    MatOption,
+    NgForOf
   ],
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
 export class UpdateProductComponent implements OnInit{
   product: Product;
-
+  categories: string[] = ['Electronics', 'Clothes', 'Books', 'Home', 'Sports'];
   constructor(
     private dialogRef: MatDialogRef<UpdateProductComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Product
